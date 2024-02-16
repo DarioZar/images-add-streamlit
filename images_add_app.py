12f6ce93c55ee6603ad89beccb3689ce9585aae2
Dario Zarcone <dariozarcone@gmail.com>
2024-02-16 20:56:05 +0100

    Code refactor. Changed image translation. Add intensity over 1.0

--- images_add_app.py
+++ images_add_app.py
@@ -2,19 +2,28 @@ import streamlit as st
 import cv2
 import numpy as np
 
+def translate_image(image, x, y):
+    # Create the translation matrix
+    M = np.float32([[1, 0, x], [0, 1, y]])
+    # Apply the affine transformation
+    return cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+
 def mean_filter(image, n):
+    # Create a kernel for the mean of n neighbours
     kernel = np.ones((n, n), np.float32) / (n*n)
+    # Apply the mean filter
     return cv2.filter2D(image, -1, kernel)
 
 def apply_image_processing(gray_image, blue_image, green_image,
-                           pump, white, posx, posy,
-                           use_mean_filter, n_mean):
-    M = np.float32([[1, 0, posx], [0, 1, posy]])
-    shifted = cv2.warpAffine(gray_image, M, (gray_image.shape[1], gray_image.shape[0]))
-
-    b = cv2.addWeighted(shifted, 1, blue_image, pump, 0)
-    g = cv2.addWeighted(shifted, 1, green_image, white, 0)
-    result_image = cv2.merge([b, g, shifted])
+                           blue, green, use_mean_filter, n_mean):
+    # Apply the translations on blue and green images
+    shiftedblue = translate_image(blue_image, blue["x"], blue["y"])
+    shiftedgreen = translate_image(green_image, green["x"], green["y"])
+    # Apply the intensity scaling
+    b = cv2.addWeighted(gray_image, 1, shiftedblue, blue["int"], 0)
+    g = cv2.addWeighted(gray_image, 1, shiftedgreen, green["int"], 0)
+    # Merge the channels
+    result_image = cv2.merge([gray_image, g, b])
 
     if use_mean_filter:
         result_image = mean_filter(result_image, n_mean)
@@ -47,22 +56,27 @@ if gray_image is not None and pump_image is not None and whitelight_image is not
     shp = gray_image.shape
 
     # Layout
-    col1, col2 = st.columns([2, 1])  # Image and Controls
+    col1, col2 = st.columns([3, 1])  # Image and Controls
 
     # Controls
     with col2:
         # Sliders
-        with st.expander("Adjust Parameters"):
-            pump = st.slider("Pump", 0.0, 1.0, 0.5)
-            white = st.slider("Whitelight", 0.0, 1.0, 0.5)
-            posx = st.slider("Translate X", -shp[0], shp[0], 0, 1)
-            posy = st.slider("Translate Y", -shp[1], shp[1], 0, 1)
+        with st.expander("Pump Parameters"):
+            pump = {}
+            pump["int"] = st.slider("Pump Intensity", 0.0, 1.5, 0.5)
+            pump["x"] = st.slider("Pump Translate X", -shp[0], shp[0], 0, 1)
+            pump["y"] = st.slider("Pump Translate Y", -shp[1], shp[1], 0, 1)
+        
+        with st.expander("Whitelight Parameters"):
+            white = {}
+            white["int"] = st.slider("Whitelight Intensity", 0.0, 1.5, 0.5)
+            white["x"] = st.slider("Whitelight Translate X", -shp[0], shp[0], 0, 1)
+            white["y"] = st.slider("Whitelight Translate Y", -shp[1], shp[1], 0, 1)
 
-            # Checkbox for mean filter
-            use_mean_filter = st.checkbox("Use Mean Filter")
-
-            # Neighbours slider
-            n_mean = st.slider("Neighbours for Mean Filter", 1, 20, 4, 1)
+        # Checkbox for mean filter
+        use_mean_filter = st.checkbox("Use Mean Filter")
+        # Neighbours slider
+        n_mean = st.slider("Neighbours for Mean Filter", 1, 20, 4, 1)
 
         st.image(gray_image, caption='Gray Image', use_column_width=True)
 
@@ -70,8 +84,7 @@ if gray_image is not None and pump_image is not None and whitelight_image is not
     with col1:
         if gray_image is not None:
             result_image = apply_image_processing(gray_image, pump_image, whitelight_image,
-                                                  pump, white, posx, posy,
-                                                  use_mean_filter, n_mean)
+                                                  pump, white, use_mean_filter, n_mean)
             # Display processed image
             st.image(result_image, caption='Processed Image', use_column_width=True)
 else:
