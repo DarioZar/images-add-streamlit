6d5ef4bfd8b0bedc831ebfe3db61c1f84f84eff3
Dario Zarcone <dariozarcone@gmail.com>
2024-02-20 18:39:41 +0100

    Add experimental methods for blending

--- images_add_app.py
+++ images_add_app.py
@@ -27,15 +27,37 @@ def mean_filter(image, n):
     return cv2.filter2D(image, -1, kernel)
 
 def apply_image_processing(gray_image, blue_image, green_image,
-                           blue, green, use_mean_filter, n_mean):
+                           blue, green, use_mean_filter, n_mean, experimental):
     # Apply the translations on blue and green images
     shiftedblue = translate_image(blue_image, blue["x"], blue["y"])
     shiftedgreen = translate_image(green_image, green["x"], green["y"])
     # Apply the intensity scaling
-    b = cv2.addWeighted(gray_image, 1, shiftedblue, blue["int"], 0)
-    g = cv2.addWeighted(gray_image, 1, shiftedgreen, green["int"], 0)
-    # Merge the channels
-    result_image = cv2.merge([gray_image, g, b])
+    if experimental["method"]=="classic":
+        b = cv2.addWeighted(gray_image, 1, shiftedblue, blue["int"], 0)
+        g = cv2.addWeighted(gray_image, 1, shiftedgreen, green["int"], 0)
+        # Merge the channels
+        result_image = cv2.merge([gray_image, g, b])
+    if experimental["method"] == "cover":
+        gray = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
+        mask = cv2.inRange(shiftedblue, (1-blue["int"]/1.5)*255, 255)
+        gray[mask != 0] = [0,0,255]
+        mask2 = cv2.inRange(shiftedgreen, (1-green["int"]/1.5)*255, 255)
+        gray[mask2 != 0] = [0,255,0]
+        result_image = gray
+    if experimental["method"] == "alpha blending":
+        gray = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2RGB)
+
+        zeros = np.zeros(shiftedblue.shape, dtype="uint8")
+        overlayblue = cv2.merge([zeros,zeros,shiftedblue])
+        overlaygreen = cv2.merge([zeros,shiftedgreen,zeros])
+
+        b = cv2.addWeighted(overlayblue, blue["int"]/1.5, gray, 1-blue["int"]/1.5, 0)
+        g = cv2.addWeighted(overlaygreen, green["int"]/1.5, gray, 1-green["int"]/1.5, 0)
+        result_image = cv2.addWeighted(b, 0.5, g, 0.5, 0)
+        
+    result_image = cv2.convertScaleAbs(result_image,
+                                       alpha=experimental["contrast"],
+                                       beta=experimental["brightness"]*255)
 
     if use_mean_filter:
         result_image = mean_filter(result_image, n_mean)
@@ -93,6 +115,12 @@ if gray_image is not None and pump_image is not None and whitelight_image is not
                 scale["pad"] = st.slider("Padding", 0., 5., 1., 0.5)
                 scale["loc"] = st.selectbox("Location", ["upper right", "upper left", "lower right", "lower left", "upper center", "lower center", "center right", "center left", "center"])
 
+        experimental = {}
+        with st.expander("Experimental"):
+            experimental["method"] = st.radio("Method", ["classic", "cover", "alpha blending"])
+            experimental["contrast"] = st.slider("Contrast", -5., 5., 1., 0.01)
+            experimental["brightness"] = st.slider("Brightness", -1., 1., 0., 0.01)
+
         # Checkbox for mean filter
         use_mean_filter = st.checkbox("Use Mean Filter")
         # Neighbours slider
@@ -104,7 +132,7 @@ if gray_image is not None and pump_image is not None and whitelight_image is not
     with col1:
         if gray_image is not None:
             result_image = apply_image_processing(gray_image, pump_image, whitelight_image,
-                                                  pump, white, use_mean_filter, n_mean)
+                                                  pump, white, use_mean_filter, n_mean, experimental)
             # Plot processed image
             fig, ax = plot_image(result_image, scale)
             st.pyplot(fig, bbox_inches='tight', pad_inches=0)
